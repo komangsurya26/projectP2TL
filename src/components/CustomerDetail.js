642da4ee76f24c4de5a790b087617eed8c504829
Komang Surya <komangsuryasedana@gmail.com>
2026-03-19 14:48:16 +0800

    feat: Integrate API data fetching for customer purchase history, monthly usage, and token trends, and update charts to display this dynamic data.

--- src/components/CustomerDetail.js
+++ src/components/CustomerDetail.js
@@ -54,7 +54,7 @@ export default function CustomerDetail({ customer, onClose }) {
     }
 
     if (customer?.meterType === "ami") {
-      monthlyUsageAmi();
+      // monthlyUsageAmi();
     }
   }, [customer]);
 
